33476a91a1400afa39d8ee09b705834ce225561f
hakkikarahan <hakkikarahan@gmail.com>
2020-06-06 23:40:00 +0300

    stillendirme, iyileştirme.

--- app.js
+++ app.js
@@ -24,11 +24,16 @@ const global = document.getElementById("global");
 const turkey = document.getElementById("turkey");
 
 
+
+
 req.onload = function () {
 
   let data = JSON.parse(this.response);
   let hepsi = data.Global;
   let turk = data.Countries.filter(item => item.Country == 'Turkey')[0];
+
+  console.log(turk);
+
 
 
 
@@ -41,24 +46,47 @@ req.onload = function () {
     global.appendChild(li);
   }) */
 
+  trhepsi = ['Yeni Vaka', 'Toplam Vaka', 'Yeni Ölüm', 'Toplam Ölüm', 'Yeni İyileşen', 'Toplam İyileşen'];
+  trturk = ['Ülke', 'Ülke Kodu', 'isim', 'Yeni Vaka', 'Toplam Vaka', 'Yeni Ölüm', 'Toplam Ölüm', 'Yeni İyileşen', 'Toplam İyileşen', 'Tarih'];;
+
+
+  // for (var [key, val] of Object.entries(hepsi)) {
+  let i = 0;
   for (var val in hepsi) {
-    // console.log(`Key: ${val} value: ${hepsi[val]}`);
-    const liw = document.createElement("li");
-    liw.textContent = `${val}: ${hepsi[val]}`
-    global.appendChild(liw);
+
+    // const liw = document.createElement("li");
+    // liw.textContent = `${val}: ${hepsi[val]}`
+    // global.appendChild(liw);
+
+    // console.log(trhepsi[i])
+    const divh = document.createElement('div');
+    divh.setAttribute('class', 'card');
+    divh.textContent = `${trhepsi[i]}: ${hepsi[val]}`
+    global.appendChild(divh);
+    i++;
   }
 
-  /* turk.forEach(item => {
-    const lit = document.createElement("li");
-    lit.textContent = `${val}: ${turk[val]}`;
-    turkey.appendChild(lit);
-  }) */
+  i = 0;
+  for (val in turk) {
+    const divt = document.createElement('div');
+    divt.setAttribute('class', 'card2');
+    divt.textContent = `${trturk[i]}: ${turk[val]}`;
+    turkey.appendChild(divt);
 
-  for (var val in turk) {
-    const lit = document.createElement("li");
+    /* const lit = document.createElement("li");
     lit.textContent = `${val}: ${turk[val]}`;
-    turkey.appendChild(lit);
+    turkey.appendChild(lit); */
+    i++;
   }
+
+
+  /* 
+    for (var val in turk) {
+      const lit = document.createElement("li");
+      lit.textContent = `${val}: ${turk[val]}`;
+      turkey.appendChild(lit);
+    } */
+
   // console.log(data.Countries.filter(item => item.Country == 'Turkey'));
 
   /* data.forEach(item => {
